5e32190c835c34b98645bdbee279494f81d56374
Wilsonabdiel <wilsonabdiel86@gmail.com>
2024-01-13 20:59:26 +0100

    feat:steup

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Custom Apps
+    'backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
